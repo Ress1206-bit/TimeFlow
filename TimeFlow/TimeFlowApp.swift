3b60493fcb07dcb6529b1909e5134d8e545a34d2
Ress1206-bit <adam.ress@icloud.com>
2025-05-29 16:14:13 -0400

    Initial Commit

---
+++ TimeFlow/TimeFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeFlowApp.swift
+//  TimeFlow
+//
+//  Created by Adam Ress on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct TimeFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
